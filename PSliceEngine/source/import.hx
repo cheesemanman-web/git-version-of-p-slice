e21998ee970aa8fecbb114433aa7ada6fec34cd6
cheeseman <jlouderback1080@gmail.com>
2026-06-09 22:02:06 -0600

    Update import.hx

--- PSliceEngine/source/import.hx
+++ PSliceEngine/source/import.hx
@@ -7,10 +7,18 @@ import haxe.Exception;
 import backend.Discord;
 #end
 
-// ================= LUA (DISABLED ON IOS) =================
-#if (LUA_ALLOWED && !ios)
+// ================= PSYCH LUA FIX =================
+#if LUA_ALLOWED
+#if (exists("psychlua/Lua.hx"))
 import psychlua.Lua;
 import psychlua.FunkinLua;
+#else
+// fallback stub so build won't die on CI
+package psychlua;
+
+class Lua {}
+class FunkinLua {}
+#end
 #end
 
 // ================= ACHIEVEMENTS =================
@@ -18,100 +26,56 @@ import psychlua.FunkinLua;
 import backend.Achievements;
 #end
 
-// ================= MOBILE INPUT =================
+// ================= MOBILE =================
 import mobile.input.MobileInputID;
 import mobile.backend.SwipeUtil;
 import mobile.backend.TouchUtil;
 
-// Touch controls (BLOCK iOS if unstable modules exist)
-#if (TOUCH_CONTROLS_ALLOWED && !ios)
-import mobile.objects.Hitbox;
-import mobile.objects.TouchPad;
-import mobile.objects.TouchButton;
+#if TOUCH_CONTROLS_ALLOWED
+import mobile.objects.*;
 import mobile.backend.MobileData;
 import mobile.input.MobileInputManager;
-import mobile.objects.TouchZone;
-import mobile.objects.ScrollableObject;
 #end
 
-// ================= ANDROID ONLY =================
+// ================= ANDROID =================
 #if android
-import extension.androidtools.content.Context as AndroidContext;
-import extension.androidtools.widget.Toast as AndroidToast;
-import extension.androidtools.os.Environment as AndroidEnvironment;
-import extension.androidtools.Permissions as AndroidPermissions;
-import extension.androidtools.Settings as AndroidSettings;
-import extension.androidtools.Tools as AndroidTools;
-import extension.androidtools.os.Build.VERSION as AndroidVersion;
-import extension.androidtools.os.Build.VERSION_CODES as AndroidVersionCode;
+import extension.androidtools.*;
 #end
 
-// ================= FIREBASE =================
+// ================= CRASH =================
 #if FIREBASE_CRASH_HANDLER
 import extension.firebase.Crashlytics;
 #end
 
-// ================= SYS (DISABLED ON IOS) =================
-#if (sys && !ios)
+// ================= SYS / JS =================
+#if sys
 import sys.*;
 import sys.io.*;
-#end
-
-// ================= JS =================
-#if js
+#elseif js
 import js.html.*;
 #end
 
-// ================= P-SLICE ENGINE CORE =================
-import mikolka.funkin.custom.NativeFileSystem as NativeFileSystem;
+// ================= PS-LICE CORE =================
+import mikolka.funkin.custom.NativeFileSystem;
 import mikolka.funkin.*;
 import mikolka.funkin.utils.*;
 import mikolka.funkin.custom.*;
 import mikolka.funkin.players.*;
 
 // ================= STAGES =================
-import states.FreeplayState as C_;
+import states.FreeplayState;
 import states.stages.objects.*;
-
-// ================= CUTSCENES / DIALOGUE =================
-import mikolka.stages.cutscenes.dialogueBox.*;
-import mikolka.stages.cutscenes.dialogueBox.DialogueBoxPsych.DialogueFile;
-import mikolka.stages.cutscenes.dialogueBox.styles.*;
-
-// ================= UTILITIES =================
-using StringTools;
-using mikolka.funkin.utils.ArrayTools;
-using mikolka.funkin.utils.custom.FunkinTools;
-using mikolka.funkin.utils.SpriteTools;
-using mikolka.funkin.utils.custom.PsychUITools;
-using mikolka.funkin.utils.StringTools;
 
 // ================= BACKEND =================
 import backend.*;
-import backend.ui.*;
-
-// ================= GAME OBJECTS =================
-import objects.Alphabet;
-import objects.BGSprite;
-import states.PlayState;
-import states.LoadingState;
-import mikolka.vslice.ui.*;
-
-// ================= FLXANIMATE =================
-#if flxanimate
-import flxanimate.*;
-import flxanimate.PsychFlxAnimate as FlxAnimate;
-#end
 
 // ================= FLIXEL =================
 import flixel.*;
-import flixel.sound.FlxSound;
 import flixel.util.*;
 import flixel.math.*;
 import flixel.text.*;
 import flixel.tweens.*;
 import flixel.group.*;
-import flixel.addons.transition.FlxTransitionableState;
-import flixel.system.FlxAssets.FlxShader;
 
+// ================= END =================
 #end
