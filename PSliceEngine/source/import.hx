3a5e43efb73196983ac73829e3b430b2ac615999
cheeseman <jlouderback1080@gmail.com>
2026-06-09 21:57:34 -0600

    Update import.hx

--- PSliceEngine/source/import.hx
+++ PSliceEngine/source/import.hx
@@ -2,26 +2,29 @@
 
 import haxe.Exception;
 
-//Discord API
+// ================= DISCORD =================
 #if DISCORD_ALLOWED
 import backend.Discord;
 #end
 
-//Psych
-#if LUA_ALLOWED
+// ================= LUA (DISABLED ON IOS) =================
+#if (LUA_ALLOWED && !ios)
 import psychlua.Lua;
 import psychlua.FunkinLua;
 #end
 
+// ================= ACHIEVEMENTS =================
 #if ACHIEVEMENTS_ALLOWED
 import backend.Achievements;
 #end
 
-// Mobile Controls
+// ================= MOBILE INPUT =================
 import mobile.input.MobileInputID;
 import mobile.backend.SwipeUtil;
 import mobile.backend.TouchUtil;
-#if TOUCH_CONTROLS_ALLOWED
+
+// Touch controls (BLOCK iOS if unstable modules exist)
+#if (TOUCH_CONTROLS_ALLOWED && !ios)
 import mobile.objects.Hitbox;
 import mobile.objects.TouchPad;
 import mobile.objects.TouchButton;
@@ -30,7 +33,8 @@ import mobile.input.MobileInputManager;
 import mobile.objects.TouchZone;
 import mobile.objects.ScrollableObject;
 #end
-// Android
+
+// ================= ANDROID ONLY =================
 #if android
 import extension.androidtools.content.Context as AndroidContext;
 import extension.androidtools.widget.Toast as AndroidToast;
@@ -40,102 +44,74 @@ import extension.androidtools.Settings as AndroidSettings;
 import extension.androidtools.Tools as AndroidTools;
 import extension.androidtools.os.Build.VERSION as AndroidVersion;
 import extension.androidtools.os.Build.VERSION_CODES as AndroidVersionCode;
-//? Is this even used???
-//import android.os.BatteryManager as AndroidBatteryManager;
 #end
 
+// ================= FIREBASE =================
 #if FIREBASE_CRASH_HANDLER
 import extension.firebase.Crashlytics;
 #end
 
-#if sys
+// ================= SYS (DISABLED ON IOS) =================
+#if (sys && !ios)
 import sys.*;
 import sys.io.*;
-#elseif js
+#end
+
+// ================= JS =================
+#if js
 import js.html.*;
 #end
 
-//P-Slice
+// ================= P-SLICE ENGINE CORE =================
 import mikolka.funkin.custom.NativeFileSystem as NativeFileSystem;
 import mikolka.funkin.*;
 import mikolka.funkin.utils.*;
 import mikolka.funkin.custom.*;
 import mikolka.funkin.players.*;
-import states.FreeplayState as C_;
 
-//P-Slice Dialouges
+// ================= STAGES =================
+import states.FreeplayState as C_;
+import states.stages.objects.*;
+
+// ================= CUTSCENES / DIALOGUE =================
 import mikolka.stages.cutscenes.dialogueBox.*;
 import mikolka.stages.cutscenes.dialogueBox.DialogueBoxPsych.DialogueFile;
 import mikolka.stages.cutscenes.dialogueBox.styles.*;
 
-//utils
+// ================= UTILITIES =================
 using StringTools;
 using mikolka.funkin.utils.ArrayTools;
 using mikolka.funkin.utils.custom.FunkinTools;
-import mikolka.funkin.utils.custom.FunkinTools;
-using mikolka.funkin.utils.ArrayTools;
 using mikolka.funkin.utils.SpriteTools;
 using mikolka.funkin.utils.custom.PsychUITools;
 using mikolka.funkin.utils.StringTools;
 
+// ================= BACKEND =================
+import backend.*;
+import backend.ui.*;
 
-
-// Stage imports (for compatibility)
-import states.stages.objects.*;
-
-import backend.Paths;
-import backend.CacheSystem;
-import backend.Controls;
-import backend.CoolUtil;
-import backend.MusicBeatState;
-import backend.MusicBeatSubstate;
-import backend.CustomFadeTransition;
-import backend.ClientPrefs;
-import backend.Conductor;
-import backend.BaseStage;
-import backend.Difficulty;
-import backend.Mods;
-import backend.Highscore;
-import backend.Language;
-
-#if !js
-import mobile.backend.StorageUtil;
-#end
-
-import backend.ui.*; //Psych-UI
-
+// ================= GAME OBJECTS =================
 import objects.Alphabet;
 import objects.BGSprite;
-
 import states.PlayState;
-import mikolka.vslice.ui.*;
 import states.LoadingState;
+import mikolka.vslice.ui.*;
 
+// ================= FLXANIMATE =================
 #if flxanimate
 import flxanimate.*;
 import flxanimate.PsychFlxAnimate as FlxAnimate;
 #end
 
-// Mod libs
-import flixel.ui.FlxBar;
-
-//Flixel
+// ================= FLIXEL =================
+import flixel.*;
 import flixel.sound.FlxSound;
-import flixel.FlxG;
-import flixel.FlxSprite;
-import flixel.FlxCamera;
-import flixel.util.FlxDestroyUtil;
-import flixel.math.FlxMath;
-import flixel.math.FlxPoint;
-import flixel.util.FlxColor;
-import flixel.util.FlxTimer;
-import flixel.text.FlxText;
-import flixel.tweens.FlxEase;
-import flixel.tweens.FlxTween;
-import flixel.group.FlxSpriteGroup;
-import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.util.*;
+import flixel.math.*;
+import flixel.text.*;
+import flixel.tweens.*;
+import flixel.group.*;
 import flixel.addons.transition.FlxTransitionableState;
 import flixel.system.FlxAssets.FlxShader;
-
 
 #end
